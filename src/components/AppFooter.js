5d31f80e5be27e40457613c637b58b4c745e352c
isadev97 <isa325697@gmail.com>
2023-08-05 15:36:06 +0530

    fetching Todo List on UI

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '../logo.svg'
 
 export function AppFooter() {
-  return (
-    <>
-    <hr />
-    <p>Created by Ishjot</p>
-    </>
-  )
+    return (
+        <>
+            <hr />
+            <p>Created in <img src={logo} style={{ height: 20, width: 20 }} /> by Ishjot</p>
+        </>
+    )
 }
--- src/components/AppMain.js
+++ src/components/AppMain.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export function AppMain() {
     const [todoName, setTodoName] = useState("")
-    const [todoList , setTodoList] = useState([])
+    const [todoList, setTodoList] = useState([])
 
-    const handleChange  = (e) =>{
+    const handleChange = (e) => {
         const currValue = e.target.value
         setTodoName(currValue)
     }
 
+    const handleSubmit = async () => {
+
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            console.log(process.env)
+            const api = `${process.env.REACT_APP_BACKEND_SERVER}/todos/drf/list`
+            const response = await axios.get(api)
+            setTodoList(response.data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <>
             <div>AppMain</div>
-            <input 
-                type="text" 
+            <input
+                type="text"
                 style={{ marginTop: 10, border: 'none', outline: '2px solid blue' }}
                 value={todoName}
                 onChange={handleChange}
             />
+            <button style={{ marginLeft: 10, background: 'yellow', border: '2px solid blue', outline: 'none', borderRadius: 10, height: 30, width: 100 }}>Add my todo !</button>
+
+            {todoList.map((todo, index) => <div key={index}>{todo.id} {todo.title}</div>)}
         </>
     )
 }
