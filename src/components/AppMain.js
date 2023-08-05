83009b077b992221da8e82d2a97c33bacd8fea57
isadev97 <isa325697@gmail.com>
2023-08-05 14:51:43 +0530

    input type text added

--- src/components/AppMain.js
+++ src/components/AppMain.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
 
 export function AppMain() {
+    const [todoName, setTodoName] = useState("")
     const [todoList , setTodoList] = useState([])
+
+    const handleChange  = (e) =>{
+        const currValue = e.target.value
+        setTodoName(currValue)
+    }
+
     return (
         <>
             <div>AppMain</div>
-            <input type="text" style={{ marginTop: 10 }} />
+            <input 
+                type="text" 
+                style={{ marginTop: 10, border: 'none', outline: '2px solid blue' }}
+                value={todoName}
+                onChange={handleChange}
+            />
         </>
     )
 }
